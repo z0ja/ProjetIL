7b090f4607e6294ababfae55d986ae5ca720dacc
youbyoub48 <ayou8@hotmail.fr>
2026-04-21 22:29:38 +0200

    ajout methode static getVignette a la classe Vignette et amelioration du parseur

--- public/assets/js/model/Vignette.js
+++ public/assets/js/model/Vignette.js
@@ -29,4 +29,37 @@ export class Vignette extends VideoPlayer {
         //this.pause();
         this.interval = setInterval(() => this.pause(),1000);
     }
+
+    static getVignette(text){
+        let ancien = ""
+        let video = ""
+        let time = ""
+        let begin_video = false
+        let begin_time = false
+        let vignette = []
+        for (let letter of text){
+            if(begin_time && letter == "]"){
+                begin_time = false
+                vignette.push([video,Number(time)])
+                video = ""
+                time = ""
+            }
+            if(begin_time) {
+                time = time+letter
+            }
+            if(begin_video && letter == ":"){
+                begin_video = false
+                begin_time = true
+            }
+            if(begin_video){
+                video = video+letter
+            }
+            if(letter == "|" && ancien == "["){
+                begin_video = true
+            }
+            ancien = letter
+        }
+        
+        return vignette
+    }
 }
